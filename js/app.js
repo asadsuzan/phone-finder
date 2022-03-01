bce990584c7a5e9ed586fcc28720c371bc5872a0
suzan <asadsuzan7@gmail.com>
2022-03-01 13:07:32 +0600

    add details page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("conected");
+// // console.log("conected");
+// const txt = document.getElementById("search-filed");
+// const btn = document.getElementById("search-btn");
+
+// btn.addEventListener("click", function () {
+//   console.log(txt.value);
+// });
