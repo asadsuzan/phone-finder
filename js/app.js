65fe0982924c1c78facf28d330bfcb718bf4224a
suzan <asadsuzan7@gmail.com>
2022-03-01 13:25:32 +0600

    load data from api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-// // console.log("conected");
-// const txt = document.getElementById("search-filed");
-// const btn = document.getElementById("search-btn");
-
-// btn.addEventListener("click", function () {
-//   console.log(txt.value);
-// });
+// load data from api
+const loadPhones = () => {
+  // selectors
+  const searchFiled = document.getElementById("search-filed");
+  const searchTxt = searchFiled.value;
+  const url = ` https://openapi.programming-hero.com/api/phones?search=${searchTxt}`;
+  //   clear search txt
+  searchFiled.value = "";
+  //   fetch data
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.status));
+  console.log(url);
+};
